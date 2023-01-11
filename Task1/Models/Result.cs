030cbb4735a71407745d1ab3955a4cb719a219aa
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2023-01-11 17:11:36 +0300

    Добавьте файлы проекта.

---
+++ Task1/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Models
+{
+    static class Result
+    {
+        public static double L_length(double a) => Math.PI * 2 * a;
+    }
+}
---
+++ Task1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task1.Models;
+
+namespace Task1
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double rcirc;
+
+        public double Rcirc
+        {
+            get => rcirc;
+            set
+            {
+                rcirc = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private double lcirc;
+
+        public double Lcirc
+        {
+            get => lcirc;
+            set
+            {
+                lcirc = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Lcirc = Result . L_length(Rcirc);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Rcirc != 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
